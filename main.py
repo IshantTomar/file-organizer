da39c6b191472ff9c47a94e43c9af9b8bbba826b
Ishant <ishanttomar26@gmail.com>
2024-03-07 21:32:34 +0530

    File detection test.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Test")
+import os
+
+p = "C:\\Users\\Admin\\desktop\\test.txt"
+
+if os.path.exists(p):
+    print("That location exists.")
+    if os.path.isfile(p):
+        print("That is a file.")
+else:
+    print("That doesn't location exists.")
