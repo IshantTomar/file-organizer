acca66ee94a330f94d8c058cfe8321a5ed4d3c12
Ishant <ishanttomar26@gmail.com>
2024-03-08 22:51:19 +0530

    Creating folder.

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import os
 
 p = "C:\\Users\\Admin\\desktop"
+def music():
+    folder_name = "Music"
+    folder_path = p
+    path = os.path.join(folder_path, folder_name)
+    return path
+
+Music_folder = music()
+
 
 check = os.listdir(p)
 
 for a in check:
-    extension = a.split('.')[-1]
-    print("[" + a + "]",end="")
-    if "." not in a:
-        print(" is a folder")
-    elif extension == "txt":
-        print(" is a text file")
-    elif extension == "url":
-        print(" is a url file")
+    # extension = a.split('.')[-1]
+    # print("[" + a + "]",end="")
+    if "Music" not in a:
+        os.mkdir(Music_folder)
     else:
-        print(" is a exe file")
+        print("That folder exist.")
+    # elif extension == "txt":
+    #     print(" is a text file")
+    # elif extension == "url":
+    #     print(" is a url file")
+    # else:
+    #     print(" is a exe file")
 
 
 
